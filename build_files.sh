583819e037b14ec36676a622537de210d86b3a13
Liben Gebremedhin <gebremedhinliben@gmail.com>
2024-04-12 11:25:13 -0700

    try

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,2 @@
-#!/bin/bash
-
-# Activate virtual environment if present
-if [ -d "/vercel/path0/.venv" ]; then
-    source /vercel/path0/.venv/bin/activate
-fi
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
-python manage.py collectstatic --no-input
+python3.9 manage.py collectstatic --noinput
