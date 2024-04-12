facbc41147f40437f0d0af7c00467c607371b943
Liben Gebremedhin <gebremedhinliben@gmail.com>
2024-04-12 14:04:48 -0700

    aaa

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+
+# Collect static files
+python manage.py collectstatic --no-input
