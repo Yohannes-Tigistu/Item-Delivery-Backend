a056c60c5cadb3945987c81a12b3beff69aa6586
Liben Gebremedhin <gebremedhinliben@gmail.com>
2024-04-12 14:47:55 -0700

    z

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
-echo "Building Project packages..."
-python3 -m pip install -r requirements.txt
+#!/usr/bin/env python
 
-python3 manage.py collectstatic -noinput
+import os
+
+# Install Pipenv if not already installed
+os.system("pip install pipenv")
+
+# Create a virtual environment and install dependencies
+os.system("pipenv install --deploy --ignore-pipfile")
+
+# Activate the virtual environment
+os.system("pipenv shell")
+
+# Run Django collectstatic (if needed)
+os.system("python manage.py collectstatic --no-input")
+
+# Deactivate the virtual environment
+os.system("exit")
