4a7afcf977279ac50015a4c088051ef76576d8c8
Liben Gebremedhin <gebremedhinliben@gmail.com>
2024-04-12 11:17:09 -0700

    new

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+#!/bin/bash
+
+# Specify Python version
+PYTHON_VERSION="python3.9"
+
+# Install dependencies
+${PYTHON_VERSION} -m pip install -r requirements.txt
+
+# Collect static files
+${PYTHON_VERSION} manage.py collectstatic
